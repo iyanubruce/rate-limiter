67a01a15373b75b23eb0cdf77a0fbbfadb31ee81
Iyanuoluwa Ikechukwu Taiwo <iyanuoluwa938@gmail.com>
2026-03-03 21:18:50 +0100

    feat: db:change fix

--- src/database/migrations/schema.ts
+++ src/database/migrations/schema.ts
@@ -1,20 +1,22 @@
-// db/schema.ts
 import {
   pgTable,
+  unique,
   serial,
   varchar,
   text,
   timestamp,
   pgEnum,
 } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
-export const roleEnum = pgEnum("user_role", ["admin", "user"]);
+export const roleEnum = pgEnum("user_role", ["admin", "moderator", "user"]);
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   email: varchar("email", { length: 255 }).notNull().unique(),
+  username: varchar("username", { length: 255 }),
   password: text("password").notNull(),
-  role: roleEnum("role").notNull().default("admin"),
+  role: roleEnum("role").default("admin"),
   first_name: varchar("first_name", { length: 255 }).notNull(),
   last_name: varchar("last_name", { length: 255 }).notNull(),
   created_at: timestamp("created_at").notNull().defaultNow(),
